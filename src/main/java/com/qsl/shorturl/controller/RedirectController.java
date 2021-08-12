f0f0ce58947a2c99fbf606886358f8beb73ac23a
qianshuailong <shuailong.qian@weimob.com>
2021-08-12 16:59:54 +0800

    refactor:调整重定向功能

--- src/main/java/com/qsl/shorturl/controller/RedirectController.java
+++ src/main/java/com/qsl/shorturl/controller/RedirectController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * 重定向 Controller层
  *
@@ -19,20 +21,36 @@ public class RedirectController {
     @Autowired
     private ShortUrlService shortUrlService;
 
-    private static final String FAKE_LONG_URL = "/test?text=welcome long url [%s]";
+    private static final String FAKE_LONG_URL = "/test?text=welcome long url %s";
 
     /**
      * 访问短网址
+     * 重定向方法一（推荐）：
+     * 使用HttpServletResponse，方便访问别的网址并可以携带参数
+     *
+     * @param shortUri 短链接
+     */
+    @ResponseBody
+    @GetMapping("/r1/{shortUri}")
+    public void visitShortUrl(@PathVariable("shortUri") String shortUri, HttpServletResponse response) throws Exception {
+        String targetUrl = shortUrlService.visitShortUrl(shortUri);
+        // todo 需重定向到真实地址，此处测试使用
+        response.sendRedirect(String.format(FAKE_LONG_URL, targetUrl));
+    }
+
+    /**
+     * 访问短网址
+     * 重定向方法二：
+     * 在返回的字符串前面添加 redirect: 方式来告诉Spring框架，需要做302重定向处理
      *
      * @param shortUri 短链接
      * @return 重定向
      */
-    @GetMapping("/{shortUri}")
-    public String visitShortUrl(@PathVariable("shortUri") String shortUri) {
-        String targetUri = shortUrlService.visitShortUrl(shortUri);
+    @GetMapping("/r2/{shortUri}")
+    public String visitShortUrl2(@PathVariable("shortUri") String shortUri) {
+        String targetUrl = shortUrlService.visitShortUrl(shortUri);
         // todo 需重定向到真实地址，此处测试使用
-        // 在返回的字符串前面添加 redirect: 方式来告诉Spring框架，需要做302重定向处理
-        return "redirect:" + String.format(FAKE_LONG_URL, targetUri);
+        return "redirect:" + String.format(FAKE_LONG_URL, targetUrl);
     }
 
     /**
